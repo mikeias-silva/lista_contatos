a9dc5753bd538281adefad9a5952de425d40adfd
msgazevedo <msgazevedo@uepg.br>
2023-05-28 17:42:03 -0300

    refactor: titles in create and edit

--- resources/views/contacts/create.blade.php
+++ resources/views/contacts/create.blade.php
@@ -2,6 +2,7 @@
 @section('content')
     <form action="{{route('contacts.store')}}" method="POST">
         @csrf
+        <h2>Create</h2>
         @include('contacts.form')
         @include('layouts.button_form')
     </form>
--- resources/views/contacts/edit.blade.php
+++ resources/views/contacts/edit.blade.php
@@ -3,6 +3,7 @@
     <form action="{{route('contacts.update', [$contact->id])}}" method="POST">
         @csrf
         @method('PUT')
+        <h2>Edit</h2>
         @include('contacts.form')
         @include('layouts.button_form')
     </form>
