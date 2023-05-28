32425c2edc6f82a8e8b5d171dd19ef4cd9c20a92
msgazevedo <msgazevedo@uepg.br>
2023-05-28 14:08:42 -0300

    feature: view and form to edit

---
+++ resources/views/contacts/edit.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+@section('content')
+    <form action="{{route('contacts.update', [$contact->id])}}" method="POST">
+        @csrf
+        @method('PUT')
+        @include('contacts.form')
+        @include('layouts.button_form')
+    </form>
+@endsection
--- resources/views/contacts/form.blade.php
+++ resources/views/contacts/form.blade.php
@@ -1,12 +1,12 @@
 @include('messages.message')
 <div>
     <label for="name">Name</label>
-    <input type="text" name="name" value="{{old('name')}}"/>
+    <input type="text" name="name" value="{{old('name') ?? $contact->name ?? ''}}"/>
 
     <label for="name">Contact</label>
-    <input type="text" name="contact" value="{{old('contact')}}"/>
+    <input type="text" name="contact" value="{{old('contact') ?? $contact->contact ?? ''}}"/>
 
     <label for="name">Email</label>
-    <input type="email" name="email" value="{{old('email')}}"/>
+    <input type="email" name="email" value="{{old('email')?? $contact->email ?? ''}}"/>
 </div>
 
